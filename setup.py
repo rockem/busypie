c9b4b2c43d9be49aca50dee76a2e9715b3848833
Eli Segal <eli.segal@gmail.com>
2019-12-20 21:30:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,16 @@ import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-requirementsPath = '%s/requirements.txt' % os.path.dirname(os.path.realpath(__file__))
-with open(requirementsPath) as f:
-    requirements = [line for line in f.read().splitlines() if len(line) > 0],
+    readme = fh.read()
 
 setup(
     name="busypie",
     version="0.0.1",
     author="Eli Segal",
     author_email="eli.segal@gmail.com",
+    license='Apache License 2.0',
     description="Busy wait easy-peasy",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/rockem/busypie",
     packages=['busypie'],
@@ -28,7 +25,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache-2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
