9ae904dd735b786a431d8d66bb80a87668126fde
Eli Segal <eli.segal@gmail.com>
2019-12-21 18:24:17 +0200

    Update test_exception.py

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -8,10 +8,10 @@ def test_should_ignore_all_exceptions():
         wait().ignore_exceptions().at_most(200, MILLISECOND).until(raise_error)
 
 
+def raise_error():
+    raise ZeroDivisionError()
+
+
 def test_fail_on_exception_if_not_ignored():
     with pytest.raises(ZeroDivisionError):
         wait().at_most(ONE_SECOND).until(raise_error)
-
-
-def raise_error():
-    raise ZeroDivisionError()
