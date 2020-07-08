fd9d08d44c1a24da9c2de576aeb5e7b07aa96452
Amos Hason <amos.hason@cs-robotics.com>
2020-07-08 16:39:29 +0300

    Revert "Revert "Temporary commit to verify failure report""
    This reverts commit fbdf1458e4fe52d7e79682261d1fb7c7fd152a12.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     readme = fh.read()
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords="busy wait tdd test builder sleep wait pytest",
     url="https://github.com/rockem/busypie",
-    packages=find_packages(exclude=['tests*']),
+    packages=["busypie"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
