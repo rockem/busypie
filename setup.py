d2f66a2783fe7a591989417366cda0f3600f76fc
Eli Segal <eli.segal@M-csr-84.lan>
2021-02-26 16:24:11 +0200

    Bumped version to 0.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="busypie",
-    version="0.4.4",
+    version="0.4.5",
     author="Eli Segal",
     author_email="eli.segal@gmail.com",
     license='Apache License 2.0',
