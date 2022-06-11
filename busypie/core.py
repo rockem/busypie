daa9ce4da8b9a18faa68dd3629804b9651de302d
Eli Segal <eli.segal@gmail.com>
2022-06-11 23:47:28 +0300

    wip

--- busypie/core.py
+++ busypie/core.py
@@ -13,6 +13,7 @@ given = wait
 def wait_at_most(value: float, unit: float = SECOND) -> 'ConditionBuilder':
     return wait().at_most(value, unit)
 
+
 set_default_timeout = busypie.condition.set_default_timeout
 
 reset_defaults = busypie.condition.reset_defaults
