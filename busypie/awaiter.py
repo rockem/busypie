0e1fab38c8fa10f758bc5766e1ea2fe25fc0a1e2
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2024-04-01 17:14:10 +0100

    introduce at_least

--- busypie/awaiter.py
+++ busypie/awaiter.py
@@ -1,8 +1,9 @@
 import asyncio
+import time
+from functools import partial
+from typing import Callable
 
 import busypie
-import time
-
 from busypie.condition import Condition
 from busypie.func import describe
 from busypie.types import Checker, ConditionEvaluator
@@ -16,25 +17,39 @@ class AsyncConditionAwaiter:
         self._last_error = None
 
     def _validate_condition(self):
-        if self._condition.poll_delay > self._condition.wait_time_in_secs:
+        if self._condition.poll_delay > self._condition.max_wait_time:
             raise ValueError('Poll delay should be shorter than maximum wait constraint')
-        if ((hasattr(self._condition, 'at_least') and self._condition.at_least)
-                >= self._condition.wait_time_in_secs):
+        if self._condition.min_wait_time >= self._condition.max_wait_time:
             raise ValueError('at least should be shorter than maximum wait constraint')
 
     async def wait_for(self, evaluator: ConditionEvaluator) -> any:
         start_time = time.time()
+        time_bounds_checker = partial(self._validate_wait_bounds, evaluator, start_time)
         await asyncio.sleep(self._condition.poll_delay)
         while True:
             try:
                 result = await self._evaluator_check(evaluator)
                 if result:
+                    time_bounds_checker(self.is_under_min_wait_time)
                     return result
             except Exception as e:
                 self._raise_exception_if_not_ignored(e)
                 self._last_error = e
-            self._validate_wait_constraint(evaluator, start_time)
+            time_bounds_checker(self.is_max_wait_time_passed)
             await asyncio.sleep(self._condition.poll_interval)
+
+    def _validate_wait_bounds(self, condition_evaluator: ConditionEvaluator, start_time: float,
+                              constraint: Callable[[float], bool]):
+        execution_time = time.time() - start_time
+        if constraint(execution_time):
+            raise busypie.ConditionTimeoutError(
+                self._describe(condition_evaluator), self._condition.max_wait_time) from self._last_error
+
+    def _describe(self, condition_evaluator: ConditionEvaluator) -> str:
+        return self._condition.description or describe(condition_evaluator)
+
+    def is_under_min_wait_time(self, execute_time: float):
+        return execute_time <= self._condition.min_wait_time
 
     def _raise_exception_if_not_ignored(self, e: Exception):
         ignored_exceptions = self._condition.ignored_exceptions
@@ -42,14 +57,8 @@ class AsyncConditionAwaiter:
                 (ignored_exceptions and e.__class__ not in ignored_exceptions):
             raise e
 
-    def _validate_wait_constraint(self, condition_evaluator: ConditionEvaluator, start_time: float):
-        execute_time = (time.time() - start_time)
-        if (hasattr(self._condition, 'at_least') and self._condition.at_least < execute_time) or execute_time > self._condition.wait_time_in_secs:
-            raise busypie.ConditionTimeoutError(
-                self._describe(condition_evaluator), self._condition.wait_time_in_secs) from self._last_error
-
-    def _describe(self, condition_evaluator: ConditionEvaluator) -> str:
-        return self._condition.description or describe(condition_evaluator)
+    def is_max_wait_time_passed(self, execute_time: float):
+        return execute_time > self._condition.max_wait_time
 
 
 class ReturnOnTimeoutAwaiter:
@@ -68,7 +77,7 @@ class ReturnOnTimeoutAwaiter:
 
 
 class ConditionTimeoutError(Exception):
-    def __init__(self, description: str, wait_time_in_secs: float):
+    def __init__(self, description: str, max_wait_time: float):
         super(ConditionTimeoutError, self).__init__("Failed to meet condition of [{}] within {} seconds"
-                                                    .format(description, wait_time_in_secs))
+                                                    .format(description, max_wait_time))
         self.description = description
--- busypie/condition.py
+++ busypie/condition.py
@@ -5,7 +5,7 @@ from typing import Type
 
 from busypie import runner
 from busypie.checker import check, negative_check, assert_check
-from busypie.durations import ONE_HUNDRED_MILLISECONDS, SECOND, MILLISECOND
+from busypie.durations import ONE_HUNDRED_MILLISECONDS, SECOND
 from busypie.time import time_value_operator
 from busypie.types import ConditionEvaluator, Checker
 
@@ -21,11 +21,11 @@ class ConditionBuilder:
         self._create_time_based_evaluatortions()
 
     def _create_time_based_evaluatortions(self):
-        self.at_most = self._time_property_evaluator_for('wait_time_in_secs')
+        self.at_most = self._time_property_evaluator_for('max_wait_time')
         self.wait_at_most = self.at_most
         self.poll_delay = self._time_property_evaluator_for('poll_delay')
         self.poll_interval = self._time_property_evaluator_for('poll_interval')
-        self.at_least = self._time_property_evaluator_for('at_least')
+        self.at_least = self._time_property_evaluator_for('min_wait_time')
 
     def _time_property_evaluator_for(self, name: str):
         return partial(time_value_operator, visitor=partial(self._time_property, name=name))
@@ -46,10 +46,6 @@ class ConditionBuilder:
 
     def with_description(self, description: str) -> 'ConditionBuilder':
         self._condition.description = description
-        return self._new_builder_with_cloned_condition()
-
-    def at_least(self, at_least: int) -> 'ConditionBuilder':
-        self._condition.at_least = at_least
         return self._new_builder_with_cloned_condition()
 
     def until(self, evaluator: ConditionEvaluator) -> any:
@@ -90,12 +86,13 @@ class ConditionBuilder:
 
 
 class Condition:
-    wait_time_in_secs = DEFAULT_MAX_WAIT_TIME
+    max_wait_time = DEFAULT_MAX_WAIT_TIME
     ignored_exceptions = None
     poll_interval = DEFAULT_POLL_INTERVAL
     poll_delay = DEFAULT_POLL_DELAY
     description = None
     return_on_timeout = False
+    min_wait_time = 0
 
     def append_exception(self, exception: Type[Exception]):
         if self.ignored_exceptions is None:
@@ -107,15 +104,16 @@ class Condition:
             return False
 
         return \
-            self.wait_time_in_secs == other.wait_time_in_secs and \
+            self.max_wait_time == other.max_wait_time and \
             self.ignored_exceptions == other.ignored_exceptions and \
             self.poll_interval == other.poll_interval and \
             self.poll_delay == other.poll_delay
 
+
 set_default_timeout = partial(
     time_value_operator,
-    visitor=partial(setattr, Condition, 'wait_time_in_secs'))
+    visitor=partial(setattr, Condition, 'max_wait_time'))
 
 
 def reset_defaults() -> None:
-    Condition.wait_time_in_secs = DEFAULT_MAX_WAIT_TIME
+    Condition.max_wait_time = DEFAULT_MAX_WAIT_TIME
